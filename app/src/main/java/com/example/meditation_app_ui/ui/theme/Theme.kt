62821bd8bac206a99768580ee3550bdae10d7cc8
Yash Raj Singh <yashrajsingh807@gmail.com>
2022-02-03 10:39:31 +0530

    Initial Commit with Some resources added

---
+++ app/src/main/java/com/example/meditation_app_ui/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.example.meditation_app_ui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Meditation_App_UITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
